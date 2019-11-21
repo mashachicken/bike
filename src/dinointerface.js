31dc4d53dbb3df38be219ac7dabc1fd246d59d95
Student <example@example.com>
2019-11-20 16:41:31 -0800

    added styling

--- src/dinointerface.js
+++ src/dinointerface.js
@@ -1,11 +1,11 @@
 export default class BikeIndex {
-  async getBike(paras, words) {
+  async getBike(city) {
     try {
-      let response = await fetch(`bikeindex.org/api/v3/search/?format=json&stolenness=proximity&location =Portland, OR`);
+      let response = await fetch('http://bikeindex.org/api/v3/search/?stolenness=proximity&location='+city);
       let jsonifiedResponse = await response.json();
       return jsonifiedResponse;
     } catch(error) {
-      console.error("there was an error with your request:" + error.message);
+      $("#bikes").html("there was an error with your request:" + error.message);
       return false;
     }
   }
--- src/main.js
+++ src/main.js
@@ -1,23 +1,31 @@
 import './styles.css';
 import $ from 'jquery';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import DinoService from "./dinointerface.js";
+import BikeIndex from "./dinointerface.js";
 $(document).ready(function() {
   $('#submit').click(function() {
-    const paras = $("#paras").val();
-    const words = $("#words").val();
+    const city = $("#city").val();
 
     (async () => {
-      let dinoService = new DinoService();
-      const response = await dinoService.getDinos(paras, words);
+      let bikeIndex = new BikeIndex();
+      const response = await bikeIndex.getBike(city);
       getElements(response);
     })();
 
     const getElements = function(response) {
-      console.log(response);
-      $("#paragraphs").html("");
-      response.forEach(function(paragraph) {
-        $("#paragraphs").append("<p>"+paragraph.join(" ")+"</p>");
+      $("#bikes").html("");
+      response.bikes.forEach(function(bike) {
+        $("#bikes").append("<li class='list-group-item'>");
+        $("#bikes").append("<ul class='list-group'>");
+        $("#bikes").append("<li class='list-group-item'>ID:  "+bike.id+ "  Serial Number: " + bike.serial +"</li>");
+        if (bike.description == null) {
+          $("#bikes").append("<li class='list-group-item'>No Description On File</li>");
+        } else {
+          $("#bikes").append("<li class='list-group-item'>Description:  "+bike.description+"</li>");
+        }
+        $("#bikes").append("<li class='list-group-item'>Model: "+bike.title+"</li>");
+        $("#bikes").append("</ul>");
+        $("#bikes").append("</li>");
       });
     };
   });
